c1b39dfdf021ed65f29d7c4e9ae5d981eaf49d5a
sean54123 <seanwilliamogara@gmail.com>
2023-03-26 23:36:02 +0100

    Fixed main loop in get data file

--- get_weather_data.py
+++ get_weather_data.py
@@ -3,12 +3,12 @@ import Adafruit_DHT
 
 
 def get_data():
-    while True:
         humidity, temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 18)
         if humidity is not None and temp is not None:
             display_string = 'Temperature = {0:0.1f}* Humidity = {1:0.1f}%'.format(temp, humidity)
             print(display_string)
             return(display_string)
 
-if __name__ == 'main': 
-    get_data()
+if __name__ == '__main__': 
+    while True:
+        get_data()
