f3ee16c017b49e42980ecbb9896ecd32e710bfbd
sean54123 <seanwilliamogara@gmail.com>
2023-03-26 23:59:13 +0100

    Removed redundant code

--- intranet_page.py
+++ intranet_page.py
@@ -1,5 +1,5 @@
 import socket
-from get_weather_data import get_data 
+from get_weather_data import get_data
 
 
 # Define socket host and port
@@ -14,6 +14,7 @@ def create_socket():
     server_socket.bind((SERVER_HOST, SERVER_PORT))
     server_socket.listen(1)
     print('Listening on port %s ...' % SERVER_PORT)
+    print(server_socket)
     return server_socket
 
 
@@ -52,9 +53,8 @@ def generate_html():
     return(html_content)
 
 server = create_socket()
-get_client_connections(server_socket=server)
 
 while True:    
-        client_con= get_client_connections(server_socket=server)
+        client_con = get_client_connections(server_socket=server)
         send_response(html=generate_html(), client_connection=client_con)
 
